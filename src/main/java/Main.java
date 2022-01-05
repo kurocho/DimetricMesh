8e1ec6356231c5e12cab8b5b1266356d90493697
RetroSuperHero <37156557+RetroSuperHero@users.noreply.github.com>
2022-01-05 19:51:23 +0100

    Moved graph 13 creation to utils

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import production.Production1;
 import production.Production12;
 import production.Production13;
 import production.Production2;
+import util.Production13Util;
 
 public class Main {
-
 
     public static void main(String[] args) {
         System.setProperty("org.graphstream.ui", "javafx");
@@ -23,58 +23,17 @@ public class Main {
         graph.applyProduction(new Production2(Production2.BreakMode.VERTICAL));
         graph.applyProduction(new Production2(Production2.BreakMode.HORIZONTAL));
 
+        graph.applyProduction(new Production12());
+        graph.applyProduction(new Production12());
+
 //        GraphUtil.displayGraph(graph, 0);
-//
 //        GraphUtil.displayGraph(graph,1);
 
-//        GraphUtil.displayGraph(graph);
+        GraphUtil.displayGraph(graph);
 
-        Graph tstGraph = new Graph();
-        Node mainNode = new Node("E", 0, 0, 0);
-        tstGraph.addNode(mainNode);
-
-        Node leftI = new Node("I", -1, -1, 0);
-        Node rightI = new Node("I", 1, -1, 0);
-        tstGraph.addNode(leftI);
-        tstGraph.addNode(rightI);
-        tstGraph.addEdge(mainNode, leftI);
-        tstGraph.addEdge(mainNode, rightI);
-
-        Node leftIChildOne = new Node("I", -2, -3, 0);
-        Node leftIChildTwo = new Node("I", -2, -5, 0);
-        tstGraph.addNode(leftIChildOne);
-        tstGraph.addNode(leftIChildTwo);
-        tstGraph.addEdge(leftI, leftIChildOne);
-        tstGraph.addEdge(leftI, leftIChildTwo);
-
-        Node leftIEOne = new Node("E", 0, -2, 0);
-        Node leftIETwo = new Node("E", 0, -4, 0);
-        Node leftIEThree = new Node("E", 0, -6, 0);
-        tstGraph.addNode(leftIEOne);
-        tstGraph.addNode(leftIETwo);
-        tstGraph.addNode(leftIEThree);
-        tstGraph.addEdge(leftIChildOne, leftIEOne);
-        tstGraph.addEdge(leftIChildOne, leftIETwo);
-        tstGraph.addEdge(leftIChildTwo, leftIETwo);
-        tstGraph.addEdge(leftIChildTwo, leftIEThree);
-        tstGraph.addEdge(leftIEOne, leftIETwo);
-        tstGraph.addEdge(leftIETwo, leftIEThree);
-
-        Node rightIChild = new Node("I", 2, -4, 0);
-        tstGraph.addNode(rightIChild);
-        tstGraph.addEdge(rightI, rightIChild);
-
-        Node rightIEOne = new Node("E", 0, -2, 0);
-        Node rightIETwo = new Node("E", 0, -6, 0);
-        tstGraph.addNode(rightIEOne);
-        tstGraph.addNode(rightIETwo);
-        tstGraph.addEdge(rightIChild, rightIEOne);
-        tstGraph.addEdge(rightIChild, rightIETwo);
-        tstGraph.addEdge(rightIEOne, rightIETwo);
-
-        tstGraph.applyProduction(new Production13());
-
-        GraphUtil.displayGraph(tstGraph);
+        Graph production13Graph = Production13Util.getGraph();
+        production13Graph.applyProduction(new Production13());
+        GraphUtil.displayGraph(production13Graph);
     }
 
 }
---
+++ src/main/java/util/Production13Util.java
@@ -0,0 +1,53 @@
+package util;
+
+import graph.Graph;
+import graph.Node;
+
+public class Production13Util {
+    public static Graph getGraph() {
+        Graph resultGraph = new Graph();
+        Node mainNode = new Node("E", 0, 0, 0);
+        resultGraph.addNode(mainNode);
+
+        Node leftI = new Node("I", -1, -1, 0);
+        Node rightI = new Node("I", 1, -1, 0);
+        resultGraph.addNode(leftI);
+        resultGraph.addNode(rightI);
+        resultGraph.addEdge(mainNode, leftI);
+        resultGraph.addEdge(mainNode, rightI);
+
+        Node leftIChildOne = new Node("I", -2, -3, 0);
+        Node leftIChildTwo = new Node("I", -2, -5, 0);
+        resultGraph.addNode(leftIChildOne);
+        resultGraph.addNode(leftIChildTwo);
+        resultGraph.addEdge(leftI, leftIChildOne);
+        resultGraph.addEdge(leftI, leftIChildTwo);
+
+        Node leftIEOne = new Node("E", 0, -2, 0);
+        Node leftIETwo = new Node("E", 0, -4, 0);
+        Node leftIEThree = new Node("E", 0, -6, 0);
+        resultGraph.addNode(leftIEOne);
+        resultGraph.addNode(leftIETwo);
+        resultGraph.addNode(leftIEThree);
+        resultGraph.addEdge(leftIChildOne, leftIEOne);
+        resultGraph.addEdge(leftIChildOne, leftIETwo);
+        resultGraph.addEdge(leftIChildTwo, leftIETwo);
+        resultGraph.addEdge(leftIChildTwo, leftIEThree);
+        resultGraph.addEdge(leftIEOne, leftIETwo);
+        resultGraph.addEdge(leftIETwo, leftIEThree);
+
+        Node rightIChild = new Node("I", 2, -4, 0);
+        resultGraph.addNode(rightIChild);
+        resultGraph.addEdge(rightI, rightIChild);
+
+        Node rightIEOne = new Node("E", 0, -2, 0);
+        Node rightIETwo = new Node("E", 0, -6, 0);
+        resultGraph.addNode(rightIEOne);
+        resultGraph.addNode(rightIETwo);
+        resultGraph.addEdge(rightIChild, rightIEOne);
+        resultGraph.addEdge(rightIChild, rightIETwo);
+        resultGraph.addEdge(rightIEOne, rightIETwo);
+
+        return resultGraph;
+    }
+}
